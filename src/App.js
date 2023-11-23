bb23c79bc39367cd638e3b35f36ff741a556540e
Lameed88 <moradeyor88@gmail.com>
2023-11-23 12:35:43 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 
 import { Container } from '@chakra-ui/react';
 import { Route, Routes } from 'react-router-dom';
+import UserPage from './components/UserPage';
+
 
 
 function App() {
@@ -8,11 +10,12 @@ function App() {
   
   return (
     <Container maxW="620px"> 
-    <Route>
+    <Routes>
       <Route path="/:username" element={<UserPage />}/>
       <Route path="/:username/post/:pid" element={<postPage />}/>
-    </Route>
- 
+    </Routes>
+    <UserPage />
+
     </Container>
  
   );
