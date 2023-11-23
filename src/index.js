0efd8a75db8b79abcb200d3b029a3fb7ffa037b4
Lameed88 <moradeyor88@gmail.com>
2023-11-23 12:08:56 +0100

    update

--- src/index.js
+++ src/index.js
@@ -4,25 +4,40 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react';
+import { mode } from "@chakra-ui/theme-tools"
+import { extendTheme } from "@chakra-ui/react" 
+import { ColorModeScript } from '@chakra-ui/color-mode'
+
 
 const styles = {
   global: (props) => ({
     body: {
-      color: module("gray.800", "whiteAlpha:900")(props),
-      bg: module("gray.100", "#101010")(props)
+      color: mode("gray.800", "whiteAlpha:900")(props),
+      bg: mode("gray.100", "#101010")(props)
     },
   })
 }
+
+const colors = {
+  gray: {
+    light: "#616161",
+    dark: "#1e1e1e"
+  }
+}
+
 
 const config = {
   initialColorMode: 'dark',
   useSystemColorMode: true,
 }
 
+const theme = extendTheme({config, styles, colors})
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ChakraProvider>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <App />
     </ChakraProvider>
   </React.StrictMode>
