0676b61bf34bbe90550f0b3d0cbbac0d7fa3e578
Lameed88 <moradeyor88@gmail.com>
2023-11-27 09:54:51 +0100

    update

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,10 +1,13 @@
 import UserHeader from "../components/UserHeader"
+import UserPosts from "../components/UserPosts"
 
 
 const UserPage = () => {
   return(
-
+<>
   <UserHeader />
+  <UserPosts />
+  </>
 
   )
  
