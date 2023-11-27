60a02497dc2a13dea3432820824f7a531d6bb0b0
Lameed88 <moradeyor88@gmail.com>
2023-11-27 10:09:52 +0100

    update

--- src/components/UserPosts.js
+++ src/components/UserPosts.js
@@ -1,11 +1,17 @@
-
+import{ Avatar } from "@chakra-ui/avatar"
 import { Link } from 'react-router-dom'
+import { Flex } from "@chakra-ui/layout"
+
 
 const UserPosts = () => {
   return 
 
     <Link to={"/aliumusa/post/1"}>
-       
+       <Flex gap={3} mb={4} py={5}>
+        <Flex >
+            <Avatar src="/post1.png" />
+        </Flex>
+       </Flex>
 
     </Link>
 
